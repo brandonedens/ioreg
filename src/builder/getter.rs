a40d33593a6ecf37d6eae68fcdc62805ae3087eb
Brandon Edens <brandonedens@gmail.com>
2016-05-12 20:26:04 -0700

    Correct unsuffixed integer literal types.

--- src/builder/getter.rs
+++ src/builder/getter.rs
@@ -138,7 +138,7 @@ fn from_primitive(cx: &ExtCtxt, path: &Vec<String>, _: &node::Reg,
         let val: u64 = v.value.node;
         let lit = cx.expr_lit(
           v.value.span,
-          ast::LitKind::Int(val, ast::UnsuffixedIntLit(ast::Plus)));
+          ast::LitKind::Int(val, ast::LitIntType::Unsuffixed));
         let arm = ast::Arm {
           attrs: vec!(),
           pats: vec!(
